b024fce781df3c2237c5ccdfc5f38efa570686f7
muttaisammy <muttaisammy@gmail.com>
2024-08-14 05:10:05 +0300

    m

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,8 +5,8 @@ export default function Education() {
         <div className="education">
         <h4>Education</h4>
         <div className="content">
-          <div className="university"><a href="https://ptu.ac.in/" target="_blank" rel="noopener noreferrer">University of Texas</a></div>
-          <div className="college"><a href="http://cgc.edu.in/" target="_blank" rel="noopener noreferrer">Dallas Texas</a></div>
+          <div className="university"><a href="https://ptu.ac.in/" target="_blank" rel="noopener noreferrer">Jomo Kenyatta University of Science & Technology</a></div>
+          <div className="college"><a href="http://cgc.edu.in/" target="_blank" rel="noopener noreferrer">Nairobi Kenya</a></div>
           <div className="grad-date">MSc Computer Science</div>
 
           <div className="university-second"><a href="https://ptu.ac.in/" target="_blank" rel="noopener noreferrer">Southern Polytechnic State University</a></div>
