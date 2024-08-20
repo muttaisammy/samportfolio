0e4a9a1827e54464018e4e166d8c5dcafa0551cc
Sam Muttai <42063584+muttaisammy@users.noreply.github.com>
2024-08-20 17:02:54 +0300

    m

---
+++ src/components/Certifications.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function Certifications() {
+    return (
+        <div className="experience">
+        <h4>Certifications</h4>
+        <div className="content">
+          <div className="exp-item">
+            <div className="job">
+              <a className="company" href="https://oracle.com/" target="_blank" rel="noopener noreferrer">Oracle Certified Professional: Java SE 11 Developer</a>
+              <div className="duration">Current</div>
+            </div>
+            <div className="title">Oracle Corp</div>
+            <div className="slides">
+            <a  href="https://oracle.com/">Link</a>
+            </div>
+            <div className="job">
+              <a className="company" href="https://oracle.com/" target="_blank" rel="noopener noreferrer">Sun Solaris Unix Admin</a>
+              <div className="duration">Current</div>
+            </div>
+            <div className="title">Sun MicroSystems</div>
+            <div className="slides">
+            <a  href="https://oracle.com/">Link</a>
+            </div>
+          </div>
+          <div className="exp-item">
+            <div className="job">
+              <a className="company" href="https://oracle.com/" target="_blank" rel="noopener noreferrer">Oracle PL/ SQL</a>
+              <div className="duration">2015</div>
+            </div>
+            <div className="title">Oracle Corp</div>
+            <div className="slides">
+            <a  href="https://oracle.com/">Link</a>
+            </div>
+            <div className="exp-item">
+            <div className="job">
+              <a className="company" href="https://kenstates.netlify.app" target="_blank" rel="noopener noreferrer">Certified Ethical Hacker (CEH)</a>
+              <div className="duration">In Progress</div>
+            </div>
+            <div className="title">Certified Ethical Hacker (CEH)</div>
+            <div className="slides">
+            <a  href="https://kenstates.netlify.app">Link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    )
+}
