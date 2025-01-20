0d05e1c9760746691c7be292b8038f6b09be94f6
Sam Muttai <muttaisammy@gmail.com>
2025-01-20 15:26:19 +0300

    m

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,22 +10,25 @@ export default function Projects() {
                 
                   <div className="project-item">
                     <a className="project-title" href="https://spot.ampath.or.ke/" target="_blank" rel="noopener noreferrer">Ampath Healthcare</a>
-                    <p className="project-desc">Developed a Java Frontend and MySQL Backend application for hosting Ampath's software</p>
+                    <p className="project-desc">Part of the team that developed a Java Frontend and MySQL Backend application for hosting Ampath's software</p>
                   </div>
                   <div className="project-item">
                     <a className="project-title" href="https://spot.ampath.or.ke/" target="_blank" rel="noopener noreferrer">Ampath Healthcare</a>
-                    <p className="project-desc">Developed an Angular Frontend and MySQL Backend hospital management system (HMIS)</p>
+                    <p className="project-desc">Part of the team that developed an Angular Frontend and MySQL Backend hospital management system (HMIS)</p>
                   </div>
                   <div className="project-item">
                     <a className="project-title" href="https://spot.ampath.or.ke/" target="_blank" rel="noopener noreferrer">Ampath Healthcare</a>
-                    <p className="project-desc">Python development for Patient default predictions</p>
+                    <p className="project-desc">Part of the team for Python development for Patient default predictions</p>
                   </div>
                   <div className="project-item">
-                    <a className="project-title" href="https://kenstates.netlify.app" target="_blank" rel="noopener noreferrer">Company Website - kenstates.netlify.app; </a>
-                    <a className="project-title" href="https://autotrac.vercel.app" target="_blank" rel="noopener noreferrer">Ecommerce Fullstack - autotrac.vercel.app; </a>
-                    <a className="project-title" href="https://kentours.netlify.app" target="_blank" rel="noopener noreferrer">Hospitality Portal - kentours.netlify.app; </a>
+                    <a className="project-title" href="https://autotrac.kenstate.co.ke" target="_blank" rel="noopener noreferrer">Ecommerce Fullstack - autotrac.kenstate.co.ke; </a>
+                    <a className="project-title" href="https://realestate.kenstate.co.ke" target="_blank" rel="noopener noreferrer">Ecommerce Fullstack - realestate.kenstate.co.ke; </a>
                     <a className="project-title" href="https://tego.co.ke" target="_blank" rel="noopener noreferrer">Telecom Portal - tego.co.ke; </a>
-                    <a className="project-title" href="https://kenstates.netlify.app" target="_blank" rel="noopener noreferrer">AI Project</a>
+                    <a className="project-title" href="https://remote.kenstate.co.ke" target="_blank" rel="noopener noreferrer">Ecommerce Fullstack - remote.kenstate.co.ke; </a>
+                    <a className="project-title" href="https://kenstates.netlify.app" target="_blank" rel="noopener noreferrer">Company Website - kenstates.netlify.app; </a>
+                    <a className="project-title" href="https://liquid.tech/" target="_blank" rel="noopener noreferrer">Fullstack Billing application- https://liquid.tech/; </a>
+
+                    <a className="project-title" href="https://kentours.netlify.app" target="_blank" rel="noopener noreferrer">Hospitality Portal - kentours.netlify.app; </a>
                     <p className="project-desc">Developed various web and mobile applications for clients</p>
                   </div>
                   <div className="project-item">
